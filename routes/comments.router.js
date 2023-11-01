e457dc3af19f4341e36e9d5636f489df803a6c2e
Jhyun Y <yjhorion@gmail.com>
2023-11-01 20:01:10 +0900

    comments.router.js1차

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -13,17 +13,130 @@ import { prisma } from '../utils/prisma/index.js';
 // 4. 댓글 삭제 API
 //     - 비밀번호를 비교하여, 동일할 때만 댓글이 **삭제**되게 하기
 
+
+
+/*
+model Reviews {
+  reviewsId Int @id @default(autoincrement()) @map("reviewsId")
+  bookTitle String @map("bookTitle")
+  title String @map("title")
+  content String @map("content")
+  starRating Int @default(10) @map("starRating")
+  author String @map("author")
+  password Int @map("password")
+
+  createdAt DateTime @default(now()) @map("createdAt")
+  updatedAt DateTime @updatedAt @map("updatedAt")
+
+  @@map("Reviews")
+}
+ */
+
+
+
+
+/*
+model Comments {
+  CommentsId Int @id@default(autoincrement()) @map("CommentsID")
+  reviewsId Int @map("reviewsId")
+  content String @map("content")
+  author String @map("author")
+  password Int @map("password")
+
+  createdAt DateTime @default(now()) @map("createdAt")
+  updatedAt DateTime @updatedAt @map("updatedAt")
+
+  @@map("Comments")
+} */
+
+
+
+
 /* 댓글 등록 */
 Router.post('/reviews/:reviewId/comments', async(req, res, next) => {
+    const { reviewId } = req.params;
 
+    const { content, author, password } = req.body;
+
+    const comment = await prisma.comments.create({
+        data : {reviewId, content, author, password }
+    })
+
+    return res.status(201).join({ data: comment })
 })
 
 /* 댓글 목록 조회 */
 Router.get('/reviews/:reviewId/comments', async(req, res, next) => {
+    const { reviewId } = req.params;
 
+    const comments = await prisma.reviews.findMany({
+        where: {reviewsId : +reviewId},
+        select: {
+            CommentsId : true,
+            reviewId : true,
+            content : true,
+            author : true,
+            createdAt: true,
+            updatedAt: true,
+        }
+    })
+    return res.status(200).json({ data : comments})
 })
 
+/* 댓글 수정 */
+Router.put('/reviews/:reviewId/comments/:commentId', async(req, res, next) => {
+    const { reviewId, commentId } = req.params;
 
+    const { content, author, password } = req.body;
+
+    const comment = await prisma.comments.findUnique({
+        where:{commentsId : +commentId}
+    })
+
+    if (!comment) {
+        return res.status(404).json({errorMessage: "댓글이 존재하지 않습니다."})
+    } else if(comment.password !== password || comment.author !== author){
+        return res.status(401).json({errorMessage: "작성자명 또는 비밀번호가 일치하지 않습니다."})
+    }
+
+    await prisma.comments.update({
+        data: { content },
+        where: {
+            commentsId : +commentId,
+            author,
+            password
+        }
+    })
+    
+    return res.status(200).json({data: "댓글 수정이 완료되었습니다."})
+})
+
+/* 댓글 삭제 */
+router.delete('/reviews/:reviewId/comments/:commentId', async(req, res, next) => {
+    const { reviewId, commentId } = req.params;
+
+    const { content, author, password } = req.body;
+
+    const comment = await prisma.comments.findUnique({
+        where:{commentsId : +commentId}
+    })
+
+    if (!comment) {
+        return res.status(404).json({errorMessage: "댓글이 존재하지 않습니다."})
+    } else if(comment.password !== password || comment.author !== author){
+        return res.status(401).json({errorMessage: "작성자명 또는 비밀번호가 일치하지 않습니다."})
+    }
+
+    await prisma.comments.delete({
+        where: {
+            commentsId : +commentId,
+            author,
+            password
+        }
+    })
+    
+    return res.status(200).json({data: "댓글 삭제가 완료되었습니다."})
+})
 
 
 
