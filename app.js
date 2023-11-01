bb9b81e451e5fdd8d78eabb0e0d288e8f3d2cb1a
jeongyy123 <147481035+jeongyy123@users.noreply.github.com>
2023-11-01 20:11:50 +0900

    Update app.js
    라우터 수정

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // app.js
 
 import express from "express";
-import PostsRouter from "./routes/posts.router.js";
+import ReviewsRouter from "./routes/reviews.router.js";
+import CommentsRouter from "./routes/comments.router.js";
 
 const app = express();
 const PORT = 3030;
 
 app.use(express.json());
-app.use("/api", [PostsRouter]);
+app.use("/api", [ReviewsRouter, CommentsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
